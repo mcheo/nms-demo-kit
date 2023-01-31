61edb52ca93bbacb28a20e4c551e5e8b28f68f7b
mcheo-nginx <79243527+mcheo-nginx@users.noreply.github.com>
2023-01-31 12:45:34 +0800

    Update start_nap.sh

--- nginx-plus/container/start_nap.sh
+++ nginx-plus/container/start_nap.sh
@@ -23,6 +23,20 @@ echo -e "
 nginx_app_protect:
   # Report interval for NGINX App Protect details - the frequency the NGINX Agent checks NGINX App Protect for changes.
   report_interval: 15s
+
+# NGINX App Protect Monitoring config
+nap_monitoring:
+  # Buffer size for collector. Will contain log lines and parsed log lines
+  collector_buffer_size: 50000
+  # Buffer size for processor. Will contain log lines and parsed log lines
+  processor_buffer_size: 50000
+  # Syslog server IP address the collector will be listening to
+  syslog_ip: "127.0.0.1"
+  # Syslog server port the collector will be listening to
+  syslog_port: 514
+  #We set to 10 so that event logs push out to NMS faster for demonstration
+  report_count: 10
+  
 " >> /etc/nginx-agent/nginx-agent.conf
 
 nginx-agent $PARM
